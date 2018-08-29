f4da8f00072f5dc970118e5d37acd9202261b637
qing <18749689868@163.com>
2018-08-29 16:15:55 +0800

    服务端

---
+++ src/main/java/com/qt/sso/SSOServerApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.qt.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangdaoq
+ *
+ * 2018年8月28日 上午11:27:34
+ */
+@SpringBootApplication
+public class SSOServerApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SSOServerApplication.class, args);
+	}
+
+}
